7ac2ba93c78113df8963abf3392a05a662637089
Mohdali69 <ali6998@protonmail.com>
2019-01-15 15:38:08 +0100

    Commit Design

--- Planning_Projection/src/planning_projection/vue/FXMLDocumentController.java
+++ Planning_Projection/src/planning_projection/vue/FXMLDocumentController.java
@@ -31,6 +31,7 @@ import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -110,6 +111,10 @@ public class FXMLDocumentController implements Initializable {
     private Label salleLabelPlaces;
     @FXML
     private Button buttonSuppromer;
+    @FXML
+    private ImageView buttonDeconnexion;
+    @FXML
+    private Rectangle rectangle;
     
     @FXML
     private void handleButtonAction(ActionEvent event) throws SQLException, InterruptedException {
@@ -257,6 +262,26 @@ public class FXMLDocumentController implements Initializable {
        public void loadProjectionPane(){ 
            ProjectionPane.setVisible(true);
         }
+       public void makeFadeOutDeco(){
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(AccueilPane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                loadDecoPane();
+            }
+        });
+        fadeTransition.play();
+        
+           
+       }
+       public void loadDecoPane(){
+           ConnexionPane.setVisible(true);
+           
+        }
 
     @FXML
     private void buttonProjectionAction(ActionEvent event) {
@@ -294,6 +319,11 @@ public class FXMLDocumentController implements Initializable {
         projection.supprimerAdministratif(listeView.getSelectionModel().getSelectedItem());
         buttonAfficheAction(event);
         
+    }
+
+    @FXML
+    private void buttonDeconnexion(MouseEvent event) {
+        makeFadeOutDeco();
     }
     
 }
